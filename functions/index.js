d17e5bef0491145e0fec5f39975635cc12ea1f24
Michael Owuor <michael.owuor@gmail.com>
2020-11-14 15:27:59 +0300

    Added new requests from the backend.

--- functions/index.js
+++ functions/index.js
@@ -16,3 +16,19 @@ exports.userDeleted = functions.auth.user().onDelete((user) => {
 	const doc = admin.firestore().collection('users').doc(user.uid);
 	return doc.delete();
 });
+
+// http callable function (adding a mando request)
+exports.addRequest = functions.https.onCall((data, context) => {
+	if (!context.auth) {
+		throw new functions.https.HttpsError('unauthenticated', 'Only authenticated users can add requests');
+	}
+
+	if (data.text.length > 34) {
+		throw new functions.https.HttpsError('invalid-argument', 'Request must be no more than 30 characters long');
+	}
+
+	return admin.firestore().collection('requests').add({
+		text: data.text,
+		upvotes: 0,
+	});
+});
