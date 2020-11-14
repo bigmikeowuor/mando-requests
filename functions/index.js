e6dd124b1deaeee5903121749ba2ea3d80c0a80a
Michael Owuor <michael.owuor@gmail.com>
2020-11-14 21:47:43 +0300

    Created a Firestore trigger function.

--- functions/index.js
+++ functions/index.js
@@ -61,3 +61,22 @@ exports.upvote = functions.https.onCall(async (data, context) => {
 		upvotes: admin.firestore.FieldValue.increment(1),
 	});
 });
+
+// firestore trigger for tracking activity
+exports.logActivities = functions.firestore.document('/{collection}/{id}').onCreate((snap, context) => {
+	console.log(snap.data);
+
+	const collection = context.params.collection;
+	const id = context.params.id;
+	const activities = admin.firestore().collection('activities');
+
+	if (collection === 'requests') {
+		return activities.add({ text: 'A new Mando request was added.' });
+	}
+
+	if (collection === 'users') {
+		return activities.add({ text: 'A new user signed up.' });
+	}
+
+	return null;
+});
