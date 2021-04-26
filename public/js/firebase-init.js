2c0994d2a53134f7c43448901726e3d01ae703b9
Michael Owuor <michael.owuor@gmail.com>
2021-04-26 15:53:39 +0300

    Updating Firebase SDK values.

--- public/js/firebase-init.js
+++ public/js/firebase-init.js
@@ -1,12 +1,11 @@
 // The web app's Firebase configuration
 var firebaseConfig = {
-	apiKey: 'AIzaSyA635aAEdNtCLI1Wp76i6mnGgyh5I_F0yI',
-	authDomain: 'mando-requests-s3.firebaseapp.com',
-	databaseURL: 'https://mando-requests-s3.firebaseio.com',
-	projectId: 'mando-requests-s3',
-	storageBucket: 'mando-requests-s3.appspot.com',
-	messagingSenderId: '205269462038',
-	appId: '1:205269462038:web:b41c0790e0cab0486a1937',
+	apiKey: 'AIzaSyBqK8PlPXm8BRtqCBuWB_U0NVpvz6sCF3s',
+	authDomain: 'mando-requests-swap.firebaseapp.com',
+	projectId: 'mando-requests-swap',
+	storageBucket: 'mando-requests-swap.appspot.com',
+	messagingSenderId: '920802875595',
+	appId: '1:920802875595:web:b77450f6617f42657aa34e',
 };
 
 // Initialize Firebase
